abdc797ba9963ff37cbed7a5b6a4aa130c3535cb
nonchris <git@chris-ge.de>
2026-01-07 02:17:35 +0100

    forgot environment.py (whooopsies)

---
+++ concert_data_thing/evnironment.py
@@ -0,0 +1,16 @@
+import os
+
+# API Configuration
+API_PORT = int(os.getenv("PORT", 13675))
+
+# Basic Auth Configuration
+BASIC_AUTH_USERNAME = os.getenv("BASIC_AUTH_USERNAME", None)
+BASIC_AUTH_PASSWORD = os.getenv("BASIC_AUTH_PASSWORD", None)
+
+# Path Configuration
+ARTIFACTS_PATH = os.getenv("ARTIFACTS_PATH", "out")
+LOG_DIR = os.getenv("LOG_DIR", None)
+
+# Garbage Collector Configuration
+GC_MAX_AGE_HOURS = int(os.getenv("GC_MAX_AGE_HOURS", "24"))
+GC_INTERVAL_MINUTES = int(os.getenv("GC_INTERVAL_MINUTES", "10"))
